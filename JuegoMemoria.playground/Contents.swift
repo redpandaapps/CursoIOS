16e14be9ca7b890e238bb103988b10f6b808c6c5
diegovillalta <d.vildel@gmail.com>
2016-01-27 00:17:48 -0600

    Assignment juego memoria

---
+++ JuegoMemoria.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+
+for s in  0...100{
+
+print(s)
+    
+    if (s % 5 == 0){
+        print("Bingo")
+    }
+    if (s % 2 == 0){
+        print("Par")
+        
+    }
+    else{
+        print("Impar")
+        
+        }
+    switch (s){
+    
+    case 
+        
+        
+    }
+
+
+}
+
+
